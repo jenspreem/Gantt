d08810a70f6e3d0ddd8fa87e287676cad98f502b
Jens-Konrad Preem <jenspreem@gmail.com>
2017-02-15 15:31:25 +0200

    starts local html creation for gantchart rows on new task creation

--- getchart.php
+++ getchart.php
@@ -43,14 +43,20 @@ float:left;
 </head>
 <body>
 
-<?php
+<!-- Create the table to house my ganttable -->
+<table id="ganttable">
+<tr>
+<th>Activity</th>
+<th>Responsible</th>
 
+<?php
+#settings
 date_default_timezone_set('Europe/Helsinki');
 error_reporting(E_ERROR | E_WARNING | E_PARSE);
 ini_set("error_log", "/tmp/php-error.log");
-
+#table identifier
 $q = intval($_GET['q']);
-
+#create connection
 $con = mysqli_connect('localhost','ganttuser1','pw1','gantt');
 if (!$con) {
     die('Could not connect: ' . mysqli_error($con));
@@ -60,32 +66,22 @@ mysqli_select_db($con,"gantt");
 $sql="SELECT * FROM user1table1 WHERE chart_id=$q";
 
 
-#getrange 
+#get max range of days
 
 $rangesql="SELECT c.calendar_date FROM calendar c JOIN(SELECT MIN(StartDate) as startday, MAX(EndDate) as endday FROM user1table1 WHERE chart_id=1) u ON c.calendar_date >= u.startday AND c.calendar_date <= u.endday";
 
 $dayarray = mysqli_fetch_all(mysqli_query($con,$rangesql), MYSQLI_NUM);
-#why my dayarray is empty for fuck sakes
-
-if (!$dayarray){echo "<p>wtf</p>";}
 
 $result = mysqli_fetch_all(mysqli_query($con,$sql), MYSQLI_NUM);
 
-#make first row
-echo "<table>
-<tr>
-<th>Activity</th>
-<th>Responsible</th>";
-
-
-#replace these with rangeloop
+#create columns for all the days that are needed
 
 foreach($dayarray as $day)
 {
 echo "<th>" . $day[0] . "</th>";
 }
 #this day array is kinda retarded but mysqli_fetch_array would only fetch teh first day
-#while mysqli_fetch_all will fetch ana rray of arrays of one members, so make a less retarded dayarray
+#while mysqli_fetch_all will fetch an array of one membered arrays, so make a less retarded dayarray
 $smalldayarray=[];
 
 foreach($dayarray as $day)
@@ -95,13 +91,13 @@ $smalldayarray[] = $day[0] ;
 
 echo "</tr>";
 
-#next rows
+#next print rows for different tasks
 foreach($result as $row) {
 echo "<tr>";
 echo "<td>" . $row[1] . "</td>";
 echo "<td>" . $row[2] . "</td>";
 
-#todo need ruudud mis j22vad start ja end day vahele vastaval activityl punasega teha?
+#color only these cells that represent activity (from start till end) others stay just empty
   for($i=0;$i<count($dayarray);$i++){
     if (strtotime($smalldayarray[$i])>=strtotime($row[3]) and strtotime($smalldayarray[$i]) <= strtotime($row[4])) {
       echo <<<EOL
@@ -118,14 +114,11 @@ echo "</tr>";
 }
 
 
-
-
-#echo "</table>";
 mysqli_close($con);
 ?>
+<!-- before closing the table Ill add a row containing the form for adding new tasks -->
+</table>
 
-<tr>
-<td colspan="6"> 
 <form name="NewEntry" >
   <input type="text" name="TaskInput" placeholder="Task" size="12">
 <input type="text" name="RespInput" placeholder="Person" size="12">
@@ -133,9 +126,8 @@ mysqli_close($con);
 <input type="text" name="EndInput" placeholder="EndDate" class="datepicker">
 <input type="button" value="Create" onclick="addTask()">
 </form>
-</td>
-</tr>
-</table>
+
+
 
 
 </body>
